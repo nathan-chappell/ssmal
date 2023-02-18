06ea4782f894da63f2a7af6a909ca306f2983e33
DESKTOP-LPKA7MV\natha <nathan.s.chappell@gmail.com>
2023-02-18 18:08:47 +0100

    processor sys tests

--- tests/instructions/sys_io_test.py
+++ tests/instructions/sys_io_test.py
@@ -15,17 +15,16 @@ from components.registers import Registers
 @pytest.mark.parametrize(
     "A,_memory,expected_out,",
     [
-        (sys_io.PREG, b'', str(Registers(A=sys_io.PREG, SP=9))),
-        (sys_io.PTOPi, b'\x00\x00\x00\x00\x01\x01\x00\x00', "257"),
-        (sys_io.PTOPx, b'\x00\x00\x00\x00\x01\x01\x00\x00', "0x00000101"),
-        (sys_io.PTOPz, b'\x41\x42\x43\x00\x01\x00\x00\x00', "ABC"),
+        (sys_io.PREG, b'\x80', str(Registers(A=sys_io.PREG, SP=9))),
+        (sys_io.PTOPi, b'\x80\x00\x00\x00\x00\x01\x01\x00\x00', "257"),
+        (sys_io.PTOPx, b'\x80\x00\x00\x00\x00\x01\x01\x00\x00', "0x00000101"),
+        (sys_io.PTOPz, b'\x80\x41\x42\x43\x00\x01\x00\x00\x00', "ABC"),
     ],
 )
-def test_PSH_POP_A(A: int, _memory: bytes, expected_out: str):
+def test_sys_io(A: int, _memory: bytes, expected_out: str):
     m = Memory()
     r = Registers(A=A, SP=9)
-    m.store(0, 0x80)
-    m.store_bytes(1, _memory)
+    m.store_bytes(0, _memory)
     cout = io.StringIO()
     cin = io.StringIO()
     sys_io.SYS(r, m, cout=cout, cin=cin)
