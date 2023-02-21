66379a1b9db2a79d2b57538498b60c15fc9153ab
Nathan Chappell <nchappell@mono.software>
2023-02-21 08:33:44 +0100

    first demo

--- src/vm.py
+++ src/vm.py
@@ -12,11 +12,14 @@ from assemblers.file_assembler import FileAssembler
 from components.registers import Registers
 from instructions.processor_ops import HaltSignal
 from processors.processor import Processor
+from util.input_file_variant import InputFileVariant
+
 
 @dataclass
 class VmConfig:
     cin: T.TextIO = sys.stdin
     cout: T.TextIO = sys.stdout
+    initial_registers: Registers = Registers()
 
 
 class VM:
@@ -30,16 +33,22 @@ class VM:
     def __init__(self, config: VmConfig = VmConfig()) -> None:
         self.config = config
         self.processor = Processor()
+        self.configure()
+
+    def configure(self):
+        self.processor.sys_io.cin = self.config.cin
+        self.processor.sys_io.cout = self.config.cout
+        self.processor.registers = self.config.initial_registers
 
     def assemble(self, filename: str):
         """assembles input_file and outputs to input_file.bin"""
+        input_file = InputFileVariant(filename)
         file_assembler = FileAssembler()
-        file_assembler.symbol_table["sys"] = b'\x80'
-        file_assembler.assemble_file(filename)
+        file_assembler.assemble_file(input_file.assembler_filename)
         _bytes = file_assembler.buffer.getvalue()
-        with open(f"{filename}.{self.OBJECT_FILE_EXT}", "wb") as f:
+        with open(input_file.object_filename, "wb") as f:
             f.write(_bytes)
-        with open(f"{filename}.{self.DEBUG_FILE_EXT}", "w") as f:
+        with open(input_file.debug_filename, "w") as f:
             _debug_info = {offset: asdict(token) for offset, token in file_assembler.source_map.items()}
             json.dump({"version": self.DEBUG_INFO_VERSION, "source_map": _debug_info}, f)
 
@@ -47,14 +56,34 @@ class VM:
         """runs input_file as binary"""
         with open(filename, "rb") as f:
             _bytes = f.read()
-        syscall = partial(sys_io.SYS, cout=self.config.cout, cin=self.config.cin)
-        self.processor.sys_io.cin = self.config.cin
-        self.processor.sys_io.cout = self.config.cout
         self.processor.memory.store_bytes(0, _bytes)
-        self.processor.registers = initial_registers
         try:
             while True:
-                # breakpoint()
                 self.processor.advance()
         except HaltSignal:
             pass
+
+
+if __name__ == "__main__":
+    import argparse
+    import sys
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-c", "--compile", action="store_true")
+    parser.add_argument("-r", "--run", action="store_true")
+    parser.add_argument("filename")
+    args = parser.parse_args(sys.argv[1:])
+
+    filename = T.cast(str, args.filename)
+
+    vm = VM()
+    if args.compile:
+        if not filename.endswith(InputFileVariant.suffix_map.assembler_file_suffix):
+            raise RuntimeError(f"{filename} does not look like a ssmal assembler file - missing .al extension")
+        vm.assemble(filename)
+    elif args.run:
+        if not filename.endswith(InputFileVariant.suffix_map.object_file_suffix):
+            raise RuntimeError(f"{filename} does not look like a ssmal object file - missing .bin extension")
+        vm.run(filename)
+    else:
+        raise RuntimeError(f"neither -compile nor -run specified")
