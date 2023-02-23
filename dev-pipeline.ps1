9f10cd419b9336e816a30a76b7b203f827c4072d
DESKTOP-LPKA7MV\natha <nathan.s.chappell@gmail.com>
2023-02-23 22:04:41 +0100

    improved messaging

--- dev-pipeline.ps1
+++ dev-pipeline.ps1
@@ -16,61 +16,88 @@
 # "Failing" the `black` command just means that black ran and some file was reformatted.
 
 
-function Bannerfy([string[]] $lines) {
-    $lines | Measure-Object -Property Length -Maximum | Set-Variable 'stats'
+function GetInspirationalMessage {
+    switch (Get-Random -Maximum 2)
+    {
+        0 { "Have a `u{1f32e}" }
+        1 { "It's a bright, shiny day!" }
+    }
+}
+
+function Bannerfy([string[]] $lines, [string]$bgColor) {
+    $_lines = $lines, '', (GetInspirationalMessage) | % { $_ }
+    $_lines | Measure-Object -Property Length -Maximum | Set-Variable 'stats'
     $leftSide = '*'
     $rightSide = '*'
     $padding = 2
     $topAndBottom = '*' * ($stats.Maximum + $leftSide.Length + $rightSide.Length + 2 * $padding)
 
-    $topAndBottom | Write-Host -BackgroundColor Blue -NoNewline
-    '' | Write-Host -BackgroundColor Black
-
-    $lines | ForEach-Object {
-        $leftSide | Write-Host -BackgroundColor Blue -NoNewline
-        ' ' * $padding | Write-Host -BackgroundColor Black -NoNewline
-        $_ | Write-Host -BackgroundColor Black -NoNewline
-        ' ' * ($stats.Maximum - $_.Length) | Write-Host -BackgroundColor Black -NoNewline
-        ' ' * $padding | Write-Host -BackgroundColor Black -NoNewline
-        $rightSide | Write-Host -BackgroundColor Blue -NoNewline
-        '' | Write-Host -BackgroundColor Black
+    function _write([string]$line, [switch]$bg) { 
+        $_params = if ($bg) { @{BackgroundColor = $bgColor } } else { @{} }
+        $line | Write-Host @_params -NoNewline 
     }
+    function _newLine { '' | Write-Host -BackgroundColor Black }
 
-    $topAndBottom | Write-Host -BackgroundColor Blue -NoNewline
-    '' | Write-Host -BackgroundColor Black
+    $_padding = ' ' * $padding
+    _write $topAndBottom -bg
+    _newLine
+
+    $_lines | ForEach-Object {
+        $_spacing = ' ' * ($stats.Maximum - $_.Length)
+        _write $leftSide
+        _write $_padding
+        _write $_
+        _write $_spacing
+        _write $_padding
+        _write $leftSide
+        _newLine
+    }
+    
+    _write $topAndBottom -bg
+    _newLine
 }
 
 $Commands = @(
     @{ 
         Command = 'black --check .';
         Module  = 'black';
-        Message = "Formatting failed.  Run 'black .' to fix." 
+        Message = 'Formatting failed.', "Run 'black .' to fix.";
     }
     @{ 
         Command = 'pyright';
         Module  = 'pyright';
-        Message = 'Type checking failed.  Fix all issues and re-run command.' 
+        Message = 'Type checking failed.', 'Fix all issues and re-run command.';
     }
     @{ 
         Command = 'pytest';
         Module  = 'pytest';
-        Message = 'Regression tests failed.  Fix all issues and re-run command.' 
+        Message = 'Regression tests failed.', 'Fix all issues and re-run command.';
     }
 )
 
 $Commands | ForEach-Object {
     python -c "import $($_.Module)"
     if ($LASTEXITCODE -ne 0) {
-        "Module import failed for: $($_.Module).  Check your environment and install requirements.txt."
+        $moduleImportFailMessage = (
+            "Module import failed for: $($_.Module).",
+            'Check your environment (default interpreter),',
+            'and install requirements.txt:',
+            '       >',
+            '       > . .\Scripts\Activate.ps1',
+            '(ssmal)> pip install -r requirements.txt',
+            '       >'
+        )
+        Bannerfy $moduleImportFailMessage -bgColor "DarkYellow"
+        exit 1
     }
     
     Invoke-Expression $_.Command
     if ($LASTEXITCODE -ne 0) {
-        $_.Message | Write-Error
+        Bannerfy $_.Message -bgColor "Red"
         exit 1
     }
 }
 
-$successMessage = 'dev-pipline completed successfully!', "Have a `u{1f32e}"
+$successMessage = 'dev-pipline completed successfully!'
 
-Bannerfy $successMessage
+Bannerfy $successMessage -bgColor "Green"
